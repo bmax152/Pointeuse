c372044e7a3a388c3aebf67ac78ccfaf95bf8122
B_Maxime <66889069+bmax152@users.noreply.github.com>
2020-11-14 10:16:53 +0100

    Menu ajouter en BaseActivity

--- app/src/main/java/mb/solo/pointeuse/BaseActivity.java
+++ app/src/main/java/mb/solo/pointeuse/BaseActivity.java
@@ -1,8 +1,13 @@
 package mb.solo.pointeuse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.mi_histo:
+                //callActivity(CategoriesActivity.class);
+                return true;
+            default: //rien du tout
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void callActivity(Class<?> cls){
+        Intent intent = new Intent(BaseActivity.this, cls);
+        startActivity(intent);
     }
 }
